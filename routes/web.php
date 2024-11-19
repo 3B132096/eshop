4ae57f240d58022673e5f11225b82f611286c82f
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-11-20 00:36:26 +0800

    每一個路由名稱的URL、HTTP方法、串接的控制器&方法

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductController::class);
+/*
+products.index:     GET|HEAD        products                            ProductController@index
+
+products.show:      GET|HEAD        products/{product}                  ProductController@show
+
+products.create:    GET|HEAD        products/create                     ProductController@create
+
+products.store:     POST            products                            ProductController@store
+
+products.edit:      GET|HEAD        products/{product}/edit             ProductController@edit
+
+products.update:    PUT|PATCH       products/{product}                  ProductController@update
+
+products.destroy:   DELETE          products/{product}                  ProductController@destroy
+*/
