0e2024de71591a5df04b35611daff5384cf51457
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-01 01:18:56 +0800

    七個路由改用七個Route::敘述

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', ProductController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
+//Route::resource('products', ProductController::class)->only([
+    //'index', 'show', 'store', 'update', 'destroy'
+//]);
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('products/create', [ProductController::class, 'create'])->name("products.create");
+Route::post('products', [ProductController::class, 'store'])->name("products.store");
+Route::get('products/{products}/edit', [ProductController::class, 'edit'])->name("products.edit");
+Route::patch('products/{product}', [ProductController::class, 'update'])->name("products.update");
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name("products.destroy");
 /*
 products.index:     GET|HEAD        products                            ProductController@index 列出所有產品
 
