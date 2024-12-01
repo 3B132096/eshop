b24d41d47cdb11b93417d908f38827743d099c4d
葉佳豪 <3B132096@gm.student.ncut.edu.tw>
2024-12-01 11:34:40 +0800

    安裝使用者身分驗證相關套件與功能

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 //Route::resource('products', ProductController::class)->only([
-    //'index', 'show', 'store', 'update', 'destroy'
+//'index', 'show', 'store', 'update', 'destroy'
 //]);
+
 Route::get('products', [ProductController::class, 'index'])->name('products.index');
 Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
 Route::get('products/create', [ProductController::class, 'create'])->name("products.create");
@@ -27,6 +30,7 @@ Route::post('products', [ProductController::class, 'store'])->name("products.sto
 Route::get('products/{products}/edit', [ProductController::class, 'edit'])->name("products.edit");
 Route::patch('products/{product}', [ProductController::class, 'update'])->name("products.update");
 Route::delete('products/{product}', [ProductController::class, 'destroy'])->name("products.destroy");
+
 /*
 products.index:     GET|HEAD        products                            ProductController@index 列出所有產品
 
@@ -42,3 +46,15 @@ products.update:    PUT|PATCH       products/{product}                  ProductC
 
 products.destroy:   DELETE          products/{product}                  ProductController@destroy 刪除某一項產品
 */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
